9d98de4744688b204a40ea589c22addfa703f996
ermeck <ermeck@bk.ru>
2019-05-28 17:14:54 +0400

    7-th ended

--- JS-desides/07 Шифр Цезаря/07.js
+++ JS-desides/07 Шифр Цезаря/07.js
@@ -1,6 +1,27 @@
 // Шифр Цезаря
 
-document.getElementById('in').onclick = function (argument) {
+// Шифруем
+document.getElementById('in').oninput = function () {
+    const offset = 3;
     let help = document.getElementById('help');
-    help.innerHTML = 1111
+    let str = this.value;
+    let out = '';
+    for (let i = 0; i < str.length; i++) {
+        let code = str.charCodeAt(i);
+        code = code + offset;
+        out += String.fromCharCode(code);
+    }
+    document.getElementById('out').innerHTML = out;
+}
+
+document.getElementById('out2').oninput = function() {
+    const offset = 3;
+    let str = this.value;
+    let out = '';
+    for (let i = 0; i < str.length; i++) {
+        let code = str.charCodeAt(i);
+        code = code - offset;
+        out += String.fromCharCode(code);
+    }
+    document.getElementById('out3').innerHTML = out;
 }
